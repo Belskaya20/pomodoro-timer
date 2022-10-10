c407264e78ba89db5cac78b05f98d2c3c9b98360
Belskaya20 <110544821+Belskaya20@users.noreply.github.com>
2022-10-10 15:24:17 +0300

    Домашняя работа - 9

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-let timerId = document.getElementById('pomodoro-time');
-let timerClock = setInterval(timerId);
+let timerClock = null;
 let start = document.getElementById('start');
 
-
 function timer() {
-    start.addEventListener('click', function() {
-        let i = 0;
-        let minute = 25;
-        let sec = 60;
-        setInterval(function () {
-            document.getElementById("pomodoro-time").innerHTML = minute + " : " + sec;
-            sec--;
-            if (sec == 00) {
-                minute--;
-                sec = 60;
-            if (minute == 0) {
-                minute = 25;
-            }
-            if(sec || minute <= 0) {
-                clearInterval(timerClock);
-            }
+start.addEventListener('click', function() {
+}
+)};
+
+timeClock = setInterval(function () {
+    let i = 0;
+    document.getElementById("pomodoro-time").innerHTML = minute + " : " + sec;
+        sec--;
+        if (sec == 00) {
+            minute--;
+            sec = 60;
+        if (minute == 0) {
+            minute = 25;
+            clearInterval(timerClock);
         }
-    }, 1000);
-});
-};
+    }
+    if (sec <= 0 && minute <= 0) {
+        clearInterval(timerClock);
+    }
+}, 1000);
 
 function changeText () {
     if (this.start == "Start") { 
