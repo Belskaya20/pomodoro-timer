977905355d7187815b9269f8ce5977622d49ad52
Belskaya20 <110544821+Belskaya20@users.noreply.github.com>
2022-10-17 13:38:58 +0300

    Домашнее задание 9

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 let timerClock = null;
-let start = document.querySelector('#start');
+let start = document.getElementById('start');
 let timerId = document.getElementById('pomodoro-time');
 
 function timer() {
     start.addEventListener('click', function () {
-        start = (start == 'Start' ? 'Stop' : 'Start');
+        start.textContent == 'Start' ? 'Stop' : 'Start';
+
+        if (start.textContent === "Stop") {
+            clearInterval(timerClock);
+        }
+
         timerClock = setInterval(function () {
-        let sec = timerId.textContent.split(":")[1];
-        let minute = timerId.textContent.split(":")[0];
-        document.getElementById('pomodoro-time').innerHTML = `${minute}:${sec}`;
-        let i = 0;
-        if (sec > 0) {
-            sec -= 1;
-        } else if (minute > 0) {
-            minute -= 1;
-            sec = 59;
-            clearInterval(timerClock);
-        }
+            let sec = timerId.textContent.split(":")[1];
+            let minute = timerId.textContent.split(":")[0];
+            document.getElementById('pomodoro-time').innerHTML = `${minute}:${sec}`;
+            let i = 0;
+            let ssec = '' + sec;
+            if (ssec.length < 2) {
+                ssec = '0' + ssec;
+            }
 
-        if (sec <= 0 && minute <= 0) {
-            clearInterval(timerClock);
-        }
+            let min = '' + minute;
+            if (min.length < 2) {
+                min = '0' + min;
+            }
 
-        document.getElementById("pomodoro-time").innerHTML = minute + " : " + sec;
-    }, 1000);
-}
+            if (sec > 0) {
+                sec -= 1;
+            } else if (minute > 0) {
+                minute -= 1;
+                sec = 59;
+            }
+
+            if (sec <= 0 && minute <= 0) {
+                clearInterval(timerClock);
+            }
+
+            document.getElementById("pomodoro-time").innerHTML = minute + " : " + sec;
+        }, 1000);
+    }
     )
 };
 timer();
